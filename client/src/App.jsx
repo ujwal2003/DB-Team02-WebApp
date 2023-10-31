127c6aafb37ae6afbb99d2a5bca95e7f7cb98771
ramirezfernando <ramirez.fernando2003@gmail.com>
2023-10-31 01:06:21 -0500

    add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import './App.css';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<div className="text-lg underline">Tailwind test</div>}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="home" element={<Home />}/>
+      </Route>
     </Routes>
     </>
   )
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-[#05204A] text-white font-semibold p-4 py-10">
+      <div className="container mx-auto">
+        <div className="flex justify-between">
+            <p>@Fall 2023, Group 2</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
