b4a8b625f4727fbd02db89dc81488b123c93e63a
ujwal2003 <joshiujwal2003@gmail.com>
2023-10-30 22:41:06 -0500

    added database initializer endpoint

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 const Pool = require('pg').Pool;
 const creds = require('./creds.json');
+const fs = require('fs');
+const path = require('path');
 
 const pool = new Pool({
     user: creds.user,
@@ -9,4 +11,26 @@ const pool = new Pool({
     database: creds.database
 });
 
-module.exports = pool;
+async function dbInitializer(req, res) {
+    try {
+        const queries = fs.readFileSync(path.join(__dirname, 'setup.sql'), 'utf-8');
+
+        const client = await pool.connect();
+        const response = await client.query(queries, (err, result) => {
+            if(err) {
+                return res.status(500).json({"sql_error_message": err});
+            }
+        });
+        client.release();
+
+        return res.status(200).send('succesfully initialized database.');
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({"status": "failed to initialize database", "error_message": error.message});
+    }
+}
+
+module.exports = {
+    pool,
+    dbInitializer
+};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {dbInitializer} = require('./config/db');
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,8 @@ const port = 8080;
 app.get('/', (req, res) => {
     res.status(200).json({"message": `server running on port ${port}`});
 });
+
+app.get('/init', dbInitializer);
 
 app.listen(port, () => {
     console.log(`connected to server on port ${port}.`);
