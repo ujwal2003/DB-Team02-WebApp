cc30338dd236ad49adc959578401249bc2761d35
daigza09 <daigza09@gmail.com>
2023-11-01 16:51:35 -0500

    updated some information

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -11,6 +11,8 @@ function Register() {
     phoneNumber: "",
   });
 
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -19,84 +21,106 @@ function Register() {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form from submitting (to avoid page reload)
+
+    // You can add your account creation logic here if needed
+
+    // Display the success message
+    setShowSuccessMessage(true);
+  };
+
   return (
     <main className="relative h-screen bg-cover" style={{ backgroundImage: `url(${HeroImage})` }}>
       <div className="absolute inset-0 backdrop-blur-sm"></div>
       <div className="container mx-auto text-center relative flex items-center justify-center h-full">
         <div className="bg-white py-14 px-40 bg-opacity-70">
-          <h1 className="text-7xl font-bold text-[#05204A] mb-4">Create an Account</h1>
-          <div className="mb-8">
-            <div className="mb-2">
-              <label className="text-xl text-[#05204A]">First Name</label>
+          {showSuccessMessage ? (
+            <div className="text-4xl font-semibold text-[#05204A] mb-4">
+              Congratulations! 
+              You successfully created your account :)
             </div>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              placeholder="First Name"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-8">
-            <div className="mb-2">
-              <label className="text-xl text-[#05204A]">Last Name</label>
-            </div>
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              placeholder="Last Name"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-8">
-            <div className="mb-2">
-              <label className="text-xl text-[#05204A]">Email</label>
-            </div>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Email"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-8">
-            <div className="mb-2">
-              <label className="text-xl text-[#05204A]">Account Pin Number</label>
-            </div>
-            <input
-              type="text"
-              name="accountPin"
-              value={formData.accountPin}
-              onChange={handleChange}
-              placeholder="Account Pin Number"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-8">
-            <div className="mb-2">
-              <label className="text-xl text-[#05204A]">Phone Number</label>
-            </div>
-            <input
-              type="tel"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              placeholder="Phone Number"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-8">
-            <div className="flex justify-center">
-              <button className="w-32 h-12 bg-[#05204A]  rounded-md text-white" type="submit">
-                Submit
-              </button>
-            </div>
-          </div>
+          ) : (
+            <>
+              <h1 className="text-7xl font-bold text-[#05204A] mb-4">Create an Account</h1>
+              <div className="mb-8">
+                <div className="mb-2">
+                  <label className="text-xl text-[#05204A]">First Name</label>
+                </div>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-8">
+                <div className="mb-2">
+                  <label className="text-xl text-[#05204A]">Last Name</label>
+                </div>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-8">
+                <div className="mb-2">
+                  <label className="text-xl text-[#05204A]">Email</label>
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-8">
+                <div className="mb-2">
+                  <label className="text-xl text-[#05204A]">Account Pin Number</label>
+                </div>
+                <input
+                  type="text"
+                  name="accountPin"
+                  value={formData.accountPin}
+                  onChange={handleChange}
+                  placeholder="Account Pin Number"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-8">
+                <div className="mb-2">
+                  <label className="text-xl text-[#05204A]">Phone Number</label>
+                </div>
+                <input
+                  type="tel"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                  placeholder="Phone Number"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-8">
+                <div className="flex justify-center">
+                  <button
+                    className="w-32 h-12 bg-[#05204A] rounded-md text-white"
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </main>
@@ -104,4 +128,3 @@ function Register() {
 }
 
 export default Register;
-
