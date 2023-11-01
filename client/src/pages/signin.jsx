b278e924e3f1f602327969e995145e1d8fedef34
daigza09 <daigza09@gmail.com>
2023-10-31 21:31:38 -0500

    created sign in page

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,85 +1,57 @@
 import React, { useState } from "react";
+import HeroImage from "../assets/HeroImage.png";
 import { Link } from "react-router-dom";
 
-function signin() {
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        phoneNumber: "",
-        preferredID: "", // this will be their customer id that they set
+function SignIn() {
+  const [formData, setFormData] = useState({
+    email: "",
+    accountPin: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // You can handle form submission here, for example, send the data to an API or perform client-side validation.
-        console.log("Form data submitted:", formData);
-    };
-
-    return (
-        <div>
-            <h2>Create Account</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>First Name:</label>
-                    <input
-                        type="text"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>Last Name:</label>
-                    <input
-                        type="text"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>Phone Number:</label>
-                    <input
-                        type="tel"
-                        name="phoneNumber"
-                        value={formData.phoneNumber}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>Preferred 4-digit ID:</label>
-                    <input
-                        type="number"
-                        name="preferredID"
-                        value={formData.preferredID}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Create Account</button>
-            </form>
-            <p>
-                Already have an account? <Link to="/login">Login</Link>
-            </p>
+  return (
+    <main className="relative h-screen bg-cover" style={{ backgroundImage: `url(${HeroImage})` }}>
+      <div className="absolute inset-0 backdrop-blur-sm"></div>
+      <div className="container mx-auto text-center relative flex items-center justify-center h-full">
+        <div className="bg-white py-14 px-40 bg-opacity-70">
+          <h1 className="text-7xl font-bold text-[#05204A] mb-4">Sign In</h1>
+          <div className="mb-8">
+            <div className="mb-2">
+              <label className="text-xl text-[#05204A]">Email</label>
+            </div>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-8">
+            <div className="mb-2">
+              <label className="text-xl text-[#05204A]">Account Pin Number</label>
+            </div>
+            <input
+              type="text"
+              name="accountPin"
+              value={formData.accountPin}
+              onChange={handleChange}
+              placeholder="Account Pin Number"
+              className="form-control"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </main>
+  );
 }
 
-export default signin;
+export default SignIn;
