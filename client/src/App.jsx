19fb4cbe27784f092b60d92394226837401eccb4
daigza09 <daigza09@gmail.com>
2023-10-31 20:50:54 -0500

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
@@ -8,15 +8,13 @@ import './App.css';
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/home" />} />
-              <Route path="home" element={<Home />}/>
-              <Route path="/Register" element={<Register />} />
-            </Route>
-        </Routes>
-      </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="home" element={<Home />}/>
+        <Route path="/Register" element={<Register />} />
+      </Route>
+    </Routes>
     </>
   )
 }
