b6b17c5d08a583f1f2c467e0f2404853fe7820cd
daigza09 <daigza09@gmail.com>
2023-11-02 13:32:34 -0500

    added the brown border

--- client/src/pages/ManageAccount.jsx
+++ client/src/pages/ManageAccount.jsx
@@ -21,37 +21,37 @@ function ManageAccount() {
       {/* Left navigation bar with background color #9DC087 */}
       <div className="bg-[#9DC087] w-1/8 p-8">
         <ul>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
+          <li className="mb-4">
+            <Link to="/dashboard" className="text-[#05204A] font-bold">Dashboard</Link>
           </li>
-          <li>
-            <Link to="/profile">Profile</Link>
+          <li className="mb-4">
+            <Link to="/profile" className="text-[#05204A] font-bold">Profile</Link>
           </li>
           {/* Add more navigation links as needed */}
         </ul>
       </div>
 
-      {/* Account information section */}
-      <div className="w-7/8 p-8">
-        <h1 className="text-2xl font-bold">Hello, {formatName(userInfo)}</h1>
+      {/* Brown rectangle outlining user information with increased height */}
+      <div className="w-[85%] p-12 border border-[#644536] border-4 my-12 mt-8">
+        <h1 className="text-6xl font-bold mb-4 text-[#644536]">Hello, {formatName(userInfo)}</h1>
         <ul>
-          <li>
-            <strong className="text-lg">First Name:</strong> {userInfo.firstName}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">First Name:</strong> {userInfo.firstName}
           </li>
-          <li>
-            <strong className="text-lg">Last Name:</strong> {userInfo.lastName}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">Last Name:</strong> {userInfo.lastName}
           </li>
-          <li>
-            <strong className="text-lg">Customer ID:</strong> {userInfo.customerId}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">Customer ID:</strong> {userInfo.customerId}
           </li>
-          <li>
-            <strong className="text-lg">Phone Number:</strong> {userInfo.phoneNumber}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">Phone Number:</strong> {userInfo.phoneNumber}
           </li>
-          <li>
-            <strong className="text-lg">Email:</strong> {userInfo.email}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">Email:</strong> {userInfo.email}
           </li>
-          <li>
-            <strong className="text-lg">Membership Type:</strong> {userInfo.membershipType}
+          <li className="mb-6">
+            <strong className="text-xl text-[#644536]">Membership Type:</strong> {userInfo.membershipType}
           </li>
         </ul>
       </div>
