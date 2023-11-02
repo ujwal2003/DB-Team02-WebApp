8ec5c67915963707fba66830b1d63ca1f9c6b196
daigza09 <daigza09@gmail.com>
2023-11-02 14:38:40 -0500

    added buttons

--- client/src/pages/ManageAccount.jsx
+++ client/src/pages/ManageAccount.jsx
@@ -7,9 +7,16 @@ function ManageAccount() {
     firstName: "John",
     lastName: "Doe",
     customerId: "123456",
-    phoneNumber: "555-555-5555",
+    phone: "555-555-5555",
     email: "johndoe@example.com",
     membershipType: "Gold",
+  };
+  const paymentInfo = {
+    paymentID: "123",
+    cardNumber: "1234-1234-1234-1234",
+    cvv: "123",
+    cardName: "John Doe",
+    expiration: "08/24",
   };
 
   function formatName(userInfo) {
@@ -18,42 +25,72 @@ function ManageAccount() {
 
   return (
     <div className="flex">
-      {/* Left navigation bar with background color #9DC087 */}
       <div className="bg-[#9DC087] w-1/8 p-8">
         <ul>
           <li className="mb-4">
-            <Link to="/dashboard" className="text-[#05204A] font-bold">Dashboard</Link>
+            <Link to="/dashboard" className="text-[#05204A] font-bold">
+              Dashboard
+            </Link>
           </li>
           <li className="mb-4">
-            <Link to="/profile" className="text-[#05204A] font-bold">Profile</Link>
+            <Link to="/profile" className="text-[#05204A] font-bold">
+              Profile
+            </Link>
           </li>
           {/* Add more navigation links as needed */}
         </ul>
       </div>
 
-      {/* Brown rectangle outlining user information with increased height */}
-      <div className="w-[85%] p-12 border border-[#644536] border-4 my-12 mt-8">
-        <h1 className="text-6xl font-bold mb-4 text-[#644536]">Hello, {formatName(userInfo)}</h1>
-        <ul>
-          <li className="mb-6">
+      <div className="w-[85%] p-10 mt-8 flex">
+        <div className="w-1/2">
+          <h1 className="text-4xl font-bold mb-8 text-[#644536]">
+            Account Information
+          </h1>
+          <div className="mb-6">
             <strong className="text-xl text-[#644536]">First Name:</strong> {userInfo.firstName}
-          </li>
-          <li className="mb-6">
+          </div>
+          <div className="mb-6">
             <strong className="text-xl text-[#644536]">Last Name:</strong> {userInfo.lastName}
-          </li>
-          <li className="mb-6">
+          </div>
+          <div className="mb-6">
             <strong className="text-xl text-[#644536]">Customer ID:</strong> {userInfo.customerId}
-          </li>
-          <li className="mb-6">
-            <strong className="text-xl text-[#644536]">Phone Number:</strong> {userInfo.phoneNumber}
-          </li>
-          <li className="mb-6">
+          </div>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">Phone Number:</strong> {userInfo.phone}
+          </div>
+          <div className="mb-6">
             <strong className="text-xl text-[#644536]">Email:</strong> {userInfo.email}
-          </li>
-          <li className="mb-6">
+          </div>
+          <div className="mb-6">
             <strong className="text-xl text-[#644536]">Membership Type:</strong> {userInfo.membershipType}
-          </li>
-        </ul>
+          </div>
+          <button className="bg-[#05204A] text-white font-bold py-2 px-4 rounded">
+            Update Account Information
+          </button>
+        </div>
+        <div className="w-1/2">
+          <h1 className="text-4xl font-bold mb-8 text-[#644536]">
+            Payment Information
+          </h1>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">Payment ID:</strong> {paymentInfo.paymentID}
+          </div>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">Card Number:</strong> {paymentInfo.cardNumber}
+          </div>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">CVV:</strong> {paymentInfo.cvv}
+          </div>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">Card Name:</strong> {paymentInfo.cardName}
+          </div>
+          <div className="mb-6">
+            <strong className="text-xl text-[#644536]">Expiration:</strong> {paymentInfo.expiration}
+          </div>
+          <button className="bg-[#05204A] text-white font-bold py-2 px-4 rounded">
+            Update Payment Information
+          </button>
+        </div>
       </div>
     </div>
   );
