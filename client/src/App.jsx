ca25df2ced2093799d0f17b59e7c02337864fb51
ramirezfernando <ramirez.fernando2003@gmail.com>
2023-11-02 17:18:20 -0500

    added update account info page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/signin";
 import ManageAccount from "./pages/ManageAccount";
 import UpdateAccountInformation from "./pages/UpdateAccountInformation";
-
+import UpdatePaymentInformation from "./pages/UpdatePaymentInformation";
 import './App.css';
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/ManageAccount" element={<ManageAccount/>} />
         <Route path="/UpdateAccountInformation" element={<UpdateAccountInformation/>} />
+        <Route path="/UpdatePaymentInformation" element={<UpdatePaymentInformation/>} />
       </Route>
     </Routes>
     </>
