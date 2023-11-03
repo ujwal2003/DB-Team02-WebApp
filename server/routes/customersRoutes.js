e97a29db622ac1af8e1daddfe0ca6e90bfe0cc85
ujwal2003 <joshiujwal2003@gmail.com>
2023-11-02 21:19:59 -0500

    user sign in validation

--- server/routes/customersRoutes.js
+++ server/routes/customersRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const customerController = require("../controllers/customerController");
 
 router.post("/register", customerController.registerNewUser);
+router.post("/login", customerController.validateSignIn);
 
 module.exports = router;
