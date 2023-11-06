1f1dc2f703c023730a3d721a2faeb69739a779e1
daigza09 <daigza09@gmail.com>
2023-11-06 10:04:05 -0600

    added a delete account button to the manage account page

--- client/src/pages/ManageAccount.jsx
+++ client/src/pages/ManageAccount.jsx
@@ -102,6 +102,12 @@ function ManageAccount() {
               Update Account Information
             </button>
           </Link>
+          <button className="bg-red-500 text-white font-bold py-2 px-4 rounded" onClick={() => {
+            if (window.confirm("Confirm Deletion of Account")) {
+              // Handle the account deletion logic here
+            }}}>
+    Delete Account
+  </button>
         </div>
         <div className="w-1/2">
           <h1 className="text-4xl font-bold mb-8 text-[#644536]">
