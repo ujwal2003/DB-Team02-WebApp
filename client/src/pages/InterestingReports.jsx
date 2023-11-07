52910e79fa92f92b8f6e683e2182d1d6c7d38ba9
daigza09 <daigza09@gmail.com>
2023-11-06 20:27:56 -0600

    udated reports page

--- client/src/pages/InterestingReports.jsx
+++ client/src/pages/InterestingReports.jsx
@@ -1,8 +1,66 @@
-import HeroImage from "../assets/HeroImage.png";
-import { Link } from "react-router-dom";
-function InterestingReports(){
-    return(
-        <h1>setting up your page :3</h1>
-    );
+import React, { useState, useEffect } from "react";
+
+function InterestingReports() {
+  const [showJoinButtons, setShowJoinButtons] = useState(false);
+  const [restaurantMenuData, setRestaurantMenuData] = useState([]);
+
+  useEffect(() => {
+    if (showJoinButtons) {
+      // Simulated dummy data
+      const dummyData = [
+        { restaurantID: 1, menuItemID: 101, price: 10.99 },
+        { restaurantID: 2, menuItemID: 102, price: 9.99 },
+        { restaurantID: 3, menuItemID: 103, price: 12.99 },
+      ];
+
+      setRestaurantMenuData(dummyData);
+    }
+  }, [showJoinButtons]);
+
+  const handleFirstRowButtonClick = () => {
+    setShowJoinButtons(true);
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center items-center text-center">
+      <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover:bg-[#0F355A]" onClick={handleFirstRowButtonClick}>
+        Table 1
+      </button>
+      <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover.bg-[#0F355A]" onClick={handleFirstRowButtonClick}>
+        Table 2
+      </button>
+      <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover.bg-[#0F355A]" onClick={handleFirstRowButtonClick}>
+        Table 3
+      </button>
+      {showJoinButtons && (
+        <div className="w-full flex flex-wrap justify-center items-center">
+          <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover.bg-[#0F355A]">
+            Join with Table 1
+          </button>
+          <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover.bg-[#0F355A]">
+            Join with Table 2
+          </button>
+          <button className="m-2 px-6 py-4 bg-[#05204A] text-white rounded hover.bg-[#0F355A]">
+            Join with Table 3
+          </button>
+        </div>
+      )}
+      {restaurantMenuData.length > 0 && (
+        <div className="w-full">
+          <h2>Restaurant Menu Data</h2>
+          <ul>
+            {restaurantMenuData.map((item, index) => (
+              <li key={index}>
+                <p>Restaurant ID: {item.restaurantID}</p>
+                <p>Menu Item ID: {item.menuItemID}</p>
+                <p>Price: {item.price}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default InterestingReports;
