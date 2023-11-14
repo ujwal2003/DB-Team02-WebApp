c94710a7494c606cf39a8ad44b893c451c52bcf5
ujwal2003 <joshiujwal2003@gmail.com>
2023-11-14 10:41:19 -0600

    route for loading unprocessed cart

--- server/routes/ordersRoutes.js
+++ server/routes/ordersRoutes.js
@@ -6,5 +6,6 @@ const ordersController = require('../controllers/ordersController');
 router.post('/add', ordersController.addToCart);
 router.delete('/remove', ordersController.removeFromCart);
 router.patch('/tip', ordersController.addTip);
+router.post('/load', ordersController.getUnprocessedUserCart);
 
 module.exports = router;
