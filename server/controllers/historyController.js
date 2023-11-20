441e9a8c2fa5fef29e384063c5c476f454971ee2
ujwal2003 <joshiujwal2003@gmail.com>
2023-11-20 13:40:57 -0600

    changed date format in orders list route

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -8,6 +8,13 @@ async function getOrdersHistory(req, res) {
         if(!ordersList.SQL_success)
             return res.status(500).json({"success": false, "error": ordersList.error});
 
+        ordersList.result.map((elm) => {
+            let elmDate = new Date(elm['orderdate']);
+            const formatDigit = (x) => x.toString().length === 1 ? '0' + x.toString() : x.toString();
+
+            return elm['orderdate'] = `${elmDate.getFullYear()}-${formatDigit(elmDate.getMonth())}-${formatDigit(elmDate.getDate())}`;
+        });
+
         return res.status(200).json({
             "success": true,
             "result": `retrieved processed orders of ${email}`,
