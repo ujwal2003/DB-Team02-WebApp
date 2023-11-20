0c848f374e3802dc763c39ccb43ba5492aba32c2
daigza09 <daigza09@gmail.com>
2023-11-20 17:40:55 -0600

    working on order history

--- client/src/pages/OrderHistory.jsx
+++ client/src/pages/OrderHistory.jsx
@@ -1,37 +1,32 @@
-import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function OrderHistory() {
-  const dummyOrders = [
-    {
-      orderId: 1,
-      orderDate: "2023-01-10",
-      totalPrice: 50.99,
-      status: "Delivered",
-    },
-    {
-      orderId: 2,
-      orderDate: "2023-01-15",
-      totalPrice: 35.49,
-      status: "Processing",
-    },
-    {
-      orderId: 3,
-      orderDate: "2023-01-20",
-      totalPrice: 75.25,
-      status: "Delivered",
-    },
-  ];
-
-  const [searchTerm, setSearchTerm] = useState("");
+  const [email, setOrderEmail] = useState("");
+  const [orders, setOrders] = useState([]);
   const orderListStyle = "text-[#05204A] font-bold";
   const otherElementsStyle = "text-[#05204A] font-semibold";
 
-  // Filter the orders based on the search term
-  const filteredOrders = dummyOrders.filter((order) =>
-    order.orderId.toString().includes(searchTerm)
-  );
+  // Fetch order history when the email changes
+  useEffect(() => {
+    const fetchOrderHistory = async () => {
+      try {
+        const response = await axios.get(`history/get/${email}`);
+        const data = response.data;
+        setOrders(data);
+      } catch (error) {
+        console.error("Error fetching order history:", error);
+        // Handle error as needed
+      }
+    };
+
+    if (email) {
+      fetchOrderHistory();
+    } else {
+      // Clear orders if the email is empty
+      setOrders([]);
+    }
+  }, [email]);
 
   return (
     <div className="w-[90%] p-10 mt-8 flex">
@@ -40,20 +35,18 @@ function OrderHistory() {
         <div className="mb-4">
           <input
             type="text"
-            placeholder="Search by Order ID"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Enter Order Email"
+            value={email}
+            onChange={(e) => setOrderEmail(e.target.value)}
             className="p-2 rounded border border-[#05204A] text-[#05204A] font-semibold"
           />
         </div>
         <div className="mb-8">
           <ul>
-            {filteredOrders.map((order) => (
+            {orders.map((order) => (
               <li key={order.orderId}>
-                <h2 className={otherElementsStyle}>Order ID: {order.orderId}</h2>
-                <p className={otherElementsStyle}>Order Date: {order.orderDate}</p>
-                <p className={otherElementsStyle}>Total Price: ${order.totalPrice}</p>
-                <p className={otherElementsStyle}>Status: {order.status}</p>
+                <p className={otherElementsStyle}>Order Date: {order.orderdate}</p>
+                <p className={otherElementsStyle}>Order Time: ${order.ordertime}</p>
               </li>
             ))}
           </ul>
