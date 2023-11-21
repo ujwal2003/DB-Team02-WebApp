4d68b475fe4681c75dc750476b2f6cf357097c75
daigza09 <daigza09@gmail.com>
2023-11-20 22:06:12 -0600

    working on order history page

--- client/src/pages/OrderHistory.jsx
+++ client/src/pages/OrderHistory.jsx
@@ -4,6 +4,12 @@ import axios from "axios";
 function OrderHistory() {
   const [email, setOrderEmail] = useState("");
   const [orders, setOrders] = useState([]);
+  const [orderDate, setOrderDate] = useState("");
+  const [orderTime, setOrderTime] = useState("");
+  const [viewDetails, setViewDetails] = useState(false);
+  const [vietReceipt, setViewReceipt] = useState(false);
+  const [receipt, setReceipt] = useState(null);
+
   const orderListStyle = "text-[#05204A] font-bold";
   const otherElementsStyle = "text-[#05204A] font-semibold";
 
@@ -16,7 +22,6 @@ function OrderHistory() {
         setOrders(data.data.result);
       } catch (error) {
         console.error("Error fetching order history:", error);
-        // Handle error as needed
       }
     };
 
@@ -27,11 +32,21 @@ function OrderHistory() {
       setOrders([]);
     }
   }, [email]);
+  // Fetch order receipt given order date, time, and use the same email
+  // Fetch order receipt given order date, time, and use the same email
+  // Fetch order receipt given order date, time, and use the same email
+  const handleButtonClick = (buttonName) => {
+    if (buttonName === "View Receipt"){
+      setViewReceipt(true);
+    } else{
+      setViewReceipt(false);
+    }
+  };
 
   return (
     <div className="w-[90%] p-10 mt-8 flex">
       <div className="w-1/2">
-        <h1 className={orderListStyle + " text-7xl mb-4"}>Order History</h1>
+        <h1 className={orderListStyle + " text-4xl mb-4"}>Order History</h1>
         <div className="mb-4">
           <input
             type="text"
@@ -47,12 +62,42 @@ function OrderHistory() {
               <li key={order.orderId}>
                 <p className={otherElementsStyle}>Order Date: {order.orderdate}</p>
                 <p className={otherElementsStyle}>Order Time: {order.ordertime}</p>
+                <p className={otherElementsStyle}>- - - - - - - - - - - - - - - - - </p>
               </li>
             ))}
           </ul>
         </div>
       </div>
+      <div className="w-1/2">
+      <h1 className={orderListStyle + " text-4xl mb-4"}> Order Details</h1>
+      <input
+        type="text"
+        placeholder="Enter Order Date"
+        value={orderDate}
+        onChange={(e) => setOrderDate(e.target.value)}
+        className="p-2 rounded border border-[#05204A] text-[#05204A] font-semibold mb-2" // Added margin-bottom
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Order Time"
+        value={orderTime}
+        onChange={(e) => setOrderTime(e.target.value)}
+        className="p-2 rounded border border-[#05204A] text-[#05204A] font-semibold mb-2"
+      />
+      <div className="mb-4">
+        <button className="text-[#05204A] font-semibold border border-[#05204A] rounded p-2 mr-2" onClick={() => handleButtonClick("View Receipt")}> 
+          View Receipt
+        </button>
+        <button className="text-[#05204A] font-semibold border border-[#05204A] rounded p-2 mr-2" onClick={() => handleButtonClick("View Receipt")}>
+          Restaurant Payments
+        </button>
+        <button className="text-[#05204A] font-semibold border border-[#05204A] rounded p-2" onClick={() => handleButtonClick("View Receipt")}> 
+          Payment Quantity
+        </button>
+      </div>
     </div>
+  </div>
   );
 }
 
